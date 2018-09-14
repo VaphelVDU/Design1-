4ff423211ab5407c6d204e1f70d9540dbca59ac7
Hanna <hanlof90@gmail.com>
2018-09-14 12:07:06 +0200

    Damage skit

---
+++ Game 1.0/Assets/Damage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    public int playerHealth;
+    public int damageAmount;
+    private Renderer rend;
+
+    void Update()
+    {
+        PlayerLife();
+    }
+    public void TakingDamage(int damageAmount)
+    {
+        health = health - damageAmount;
+        Print("Character damaged");
+    }
+    void PlayerLife()
+    {
+        if (health <= 0 && respawning == false)
+        {
+            respawning = true;
+            print("now dead");
+            StartCoroutine(Respawn());
+
+        }
+    }
+    public IEnumerator Respawn()
+    {
+        Print("started respawn");
+        yield return new WaitForSeconds(seconds);
+        Print("respawning");
+
+
+        transform.position = spawnPoint.position;
+        //switchCamScript.Reset();
+
+        playerHealth = 1;
+        respawning = false;
+    }
+}
+
+
