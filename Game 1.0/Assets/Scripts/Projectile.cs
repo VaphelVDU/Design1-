fddb9f8da2b54e71a579e342405aea72d20662c7
Ylva Molin <ylva.molin@elev.ga.lbs.se>
2018-09-14 10:23:32 +0200

    Ändring i Projectile script

---
+++ Game 1.0/Assets/Scripts/Projectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D projectile;
+    public Transform Spawnpoint;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Rigidbody2D clone;
+
+            clone = (Rigidbody2D)Instantiate(projectile, Spawnpoint.position, projectile.rotation);
+
+            clone.velocity = Spawnpoint.TransformDirection(Vector2.forward * 20);
+        }
+    }
+}
