2599a9eeb86ccdef4030c569c466e35a26017255
Ylva Molin <ylva.molin@elev.ga.lbs.se>
2018-09-21 10:40:12 +0200

    New projectile script

---
+++ Game 1.0/Assets/Scripts/LaunchProjectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class LaunchProjectile : MonoBehaviour
+{
+    public GameObject bullet;
+
+    public void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            GameObject b = (GameObject)(Instantiate(bullet, transform.position + transform.right * 1, 5f, Quaternion.identity));
+            b.GetComponent<Rigidbody2D>().AddForce(transform.right * 1000);
+        }
+    }
+
+}
